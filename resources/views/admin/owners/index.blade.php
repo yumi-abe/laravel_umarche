9aa9765f78baab01ba49fde5b18224a42ca647a9
yumi-abe <yumibuhibuhi0610@gmail.com>
2024-04-04 23:12:32 +0900

    ルート・ビュー編集

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -8,11 +8,11 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+                <div class="md:-6 bg-white border-b border-gray-200">
 
 
                     <section class="text-gray-600 body-font">
-                        <div class="container px-5 mx-auto">
+                        <div class="container md:px-5 mx-auto">
                           <x-flash-message status="session('status')" />
                             <div class="flex justify-end mb-4">
                                 <button onclick="location.href='{{ route('admin.owners.create') }}'" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録する</button>
@@ -31,16 +31,16 @@
                               <tbody>
                                 @foreach ($owners as $owner)
                                 <tr>
-                                  <td class="px-4 py-3">{{ $owner->name }}</td>
-                                  <td class="px-4 py-3">{{ $owner->email }}</td>
-                                  <td class="px-4 py-3">{{ $owner->created_at->diffForHumans() }}</td>
-                                  <td class="px-4 py-3">
+                                  <td class="md:px-4 py-3">{{ $owner->name }}</td>
+                                  <td class="md:px-4 py-3">{{ $owner->email }}</td>
+                                  <td class="md:px-4 py-3">{{ $owner->created_at->diffForHumans() }}</td>
+                                  <td class="md:px-4 py-3">
                                     <button onclick="location.href='{{ route('admin.owners.edit',['owner' => $owner->id]) }}'" class="text-white bg-indigo-400 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-500 rounded ">編集</button>
                                   </td>
                                   <form id="delete_{{$owner->id}}" method="post" action="{{ route('admin.owners.destroy', ['owner' => $owner->id]) }}">
                                     @csrf
                                     @method('delete')
-                                    <td class="px-4 py-3">
+                                    <td class="md:px-4 py-3">
                                       <a href="#" data-id="{{ $owner->id }}" onclick="deletePost(this)" class="text-white bg-red-400 border-0 py-2 px-4 focus:outline-none hover:bg-red-500 rounded ">削除</a>
                                     </td>
                                 </form>
