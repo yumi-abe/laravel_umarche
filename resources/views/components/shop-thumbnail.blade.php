09582b539db535d0036d0cd5b513b2b2e6c10815
yumi-abe <yumibuhibuhi0610@gmail.com>
2024-04-07 14:13:28 +0900

    sec06

--- resources/views/components/shop-thumbnail.blade.php
+++ resources/views/components/shop-thumbnail.blade.php
@@ -1,7 +1,7 @@
 <div>
-    @if(empty($shop->filename))
+    @if(empty($filename))
         <img src="{{ asset('images/no_image.jpg') }}">
     @else
-    <img src="{{ asset('storage/shops/' .$shop->filename) }}">
+    <img src="{{ asset('storage/shops/' .$filename) }}">
     @endif
 </div>
