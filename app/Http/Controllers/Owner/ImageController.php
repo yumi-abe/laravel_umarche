d0ab8575d461f31a21febc2607a06c11296d03f0
yumi-abe <yumibuhibuhi0610@gmail.com>
2024-04-11 22:40:17 +0900

    image index

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -4,17 +4,36 @@ namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Image;
+use Illuminate\Support\Facades\Auth;
 
 class ImageController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function __construct()
+    {
+    $this->middleware('auth:owners');
+
+    $this->middleware(function($request, $next) {
+        $id = $request->route()->parameter('image'); 
+        if(!is_null($id)){ // null判定
+            $imagesOwnerId = Image::findOrFail($id)->owner->id;
+             $imageId = (int)$imagesOwnerId; // キャスト 文字列→数値に型変換
+             if($imageId !== Auth::id()){ // 同じでなかったら
+             abort(404); // 404画面表示
+             }
+        }
+
+        return $next($request);
+    });
+    }
+
     public function index()
     {
-        //
+        $images = Image::where('owner_id', Auth::id())
+        ->orderBy('update_at', 'desc')
+        ->paginate(20);
+
+        return view('owner.images.index', compact('images'));
     }
 
     /**
