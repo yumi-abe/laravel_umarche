9238ae4a1e7935ab6af47da304bbc5aaac737a0a
yumi-abe <yumibuhibuhi0610@gmail.com>
2024-04-17 13:47:13 +0900

    route修正

--- app/Http/Controllers/Owner/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Owner/Auth/AuthenticatedSessionController.php
@@ -54,6 +54,6 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/login');
+        return redirect('/owner/login');
     }
 }
