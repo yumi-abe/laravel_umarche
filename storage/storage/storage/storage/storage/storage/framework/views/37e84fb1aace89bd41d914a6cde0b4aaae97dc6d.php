71ae42b899f3978859408a01f50a1d6448e04160
yumi-abe <yumibuhibuhi0610@gmail.com>
2024-04-18 11:53:31 +0900

    storage delete

--- storage/storage/storage/storage/storage/storage/framework/views/37e84fb1aace89bd41d914a6cde0b4aaae97dc6d.php
+++
@@ -1,2 +0,0 @@
-<img src="<?php echo e(asset("images/logo.png")); ?>">
-<?php /**PATH C:\xampp\htdocs\laravel\umarche\resources\views/components/application-logo.blade.php ENDPATH**/ ?>
--- storage/storage/storage/storage/storage/storage/storage/storage/storage/storage/storage/storage/storage/storage/storage/storage/storage/framework/views/abcc92cbb1e7baca16eac2fcfc3d373dfc6df5fa.php
+++
@@ -1,7 +0,0 @@
-<div>
-    <?php if(@empty($filename)): ?>
-        <img src="<?php echo e(asset('images/no_image.jpg')); ?>">
-    <?php else: ?>
-    <img src="<?php echo e(asset('storage/shops/' .$filename)); ?>">
-    <?php endif; ?>
-</div><?php /**PATH C:\xampp\htdocs\laravel\umarche\resources\views/components/shop-thumbnail.blade.php ENDPATH**/ ?>
