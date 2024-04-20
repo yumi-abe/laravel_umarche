8b60873d911a47e84e8f17e6e0fec682275668fd
yumi-abe <yumibuhibuhi0610@gmail.com>
2024-04-20 21:31:06 +0900

    cart add

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComponentTestController;
 use App\Http\Controllers\LifeCycleTestController;
-use App\Http\Controllers\User\ItemController;
+use App\Http\Controllers\User\CartController;
 
+use App\Http\Controllers\User\ItemController;
+use App\Models\Cart;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,6 +29,11 @@ Route::get('/', [ItemController::class,'index'])->name('items.index');
 
 Route::get('show/{item}',[ItemController::class, 'show'])->name('items.show');
 
+Route::prefix('cart')->middleware('auth:users')->group(function(){
+    Route::get('/', [CartController::class, 'index'])->name('cart.index');
+    Route::post('add', [CartController::class, 'add'])->name('cart.add');   
+    // Route::post('delete/{item}', [CartController::class, 'delete'])->name('cart.delete');
+});
 
 // Route::get('/dashboard', function () {
 //     return view('user.dashboard');
