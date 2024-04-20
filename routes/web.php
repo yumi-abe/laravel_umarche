7b16a0481708beb2a933d6ba001c698bbb64d4dd
yumi-abe <yumibuhibuhi0610@gmail.com>
2024-04-20 22:21:26 +0900

    cart delete

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::get('show/{item}',[ItemController::class, 'show'])->name('items.show');
 Route::prefix('cart')->middleware('auth:users')->group(function(){
     Route::get('/', [CartController::class, 'index'])->name('cart.index');
     Route::post('add', [CartController::class, 'add'])->name('cart.add');   
-    // Route::post('delete/{item}', [CartController::class, 'delete'])->name('cart.delete');
+    Route::post('delete/{item}', [CartController::class, 'delete'])->name('cart.delete');
 });
 
 // Route::get('/dashboard', function () {
