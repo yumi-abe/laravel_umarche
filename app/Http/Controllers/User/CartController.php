382760538d6051216c87b3b50676a004724b471f
yumi-abe <yumibuhibuhi0610@gmail.com>
2024-04-20 22:09:28 +0900

    cartIndex

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -10,6 +10,22 @@ use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
+    public function index()
+    {
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;
+        $totalPrice = 0;
+
+        foreach($products as $product){
+            $totalPrice += $product->price * $product->pivot->quantity;
+        }
+
+        // dd($products, $totalPrice);
+
+        return view('user.cart',
+        compact('products', 'totalPrice'));
+    }
+
     public function add(Request $request)
     {
         $itemInCart = Cart::where('product_id', $request->product_id)
@@ -26,6 +42,6 @@ class CartController extends Controller
                 'quantity' => $request->quantity
             ]);
         }
-        dd('テスト');
+        return redirect()->route('user.cart.index');
     }
 }
