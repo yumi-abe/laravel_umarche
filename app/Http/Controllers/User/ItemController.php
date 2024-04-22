0f8615f004544def84191b7e033230ba0b59428a
yumi-abe <yumibuhibuhi0610@gmail.com>
2024-04-22 22:53:49 +0900

    キュー

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -7,9 +7,10 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Stock;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Mail;
 use App\Models\PrimaryCategory;
+use Illuminate\Support\Facades\Mail;
 use App\Mail\TestMail;
+use App\Jobs\SendThanksMail;
 
 class ItemController extends Controller
 {
@@ -34,8 +35,12 @@ class ItemController extends Controller
     public function index(Request $request)
     {
         // dd($request);
-        Mail::to('test@example.com')
-        ->send(new TestMail());
+        //同期的に送信
+        // Mail::to('test@example.com')
+        // ->send(new TestMail());
+
+        //非同期的に送信
+        SendThanksMail::dispatch();
 
         $categories = PrimaryCategory::with('secondary')
         ->get();
