5a665deb814c2db7826546e8496725dcf516a7b2
yumi-abe <yumibuhibuhi0610@gmail.com>
2024-04-22 22:33:54 +0900

    testmail

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -7,7 +7,9 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Stock;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 use App\Models\PrimaryCategory;
+use App\Mail\TestMail;
 
 class ItemController extends Controller
 {
@@ -32,6 +34,8 @@ class ItemController extends Controller
     public function index(Request $request)
     {
         // dd($request);
+        Mail::to('test@example.com')
+        ->send(new TestMail());
 
         $categories = PrimaryCategory::with('secondary')
         ->get();
